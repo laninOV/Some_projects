83ccd0e33fd0e82f75d7e01a47d4aec9617b5451
laninOV <71662660+laninOV@users.noreply.github.com>
2022-08-17 10:00:05 +0300

    структура для описание местоположения шарика

---
+++ FriskyBall/point.h
@@ -0,0 +1,32 @@
+#ifndef POINT_H
+#define POINT_H
+#include <iostream>
+#include <cmath>
+
+struct Point{
+    // Инициализируем переменные
+    float pointX = 0;
+    float pointY = 0;
+
+public:
+    // Присваиваем реализацию по умолчанию
+    Point() = default;
+
+    /*  Создаём конструктор Point с списком инициализации членов.
+     *  Используется как для инициализации членов класса,
+     *  так и для вызова конструктора базового/базовых классов*/
+    Point(float x, float y): pointX(x), pointY(y)
+    {
+    }
+
+    Point operator+(const Point& other) const{ //?????????
+        return {pointX + other.pointX, pointY + other.pointY};
+    }
+
+    Point& operator+=(const Point& other){
+        pointX += other.pointX;
+        pointY += other.pointY;
+        return *this;
+    }
+};
+#endif // POINT_H
