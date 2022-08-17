07c4c381613fafeaacf1aff749df4ae7fab4d341
laninOV <71662660+laninOV@users.noreply.github.com>
2022-08-17 09:59:41 +0300

    размер шарика и его граница

---
+++ FriskyBall/geometryball.cpp
@@ -0,0 +1,17 @@
+#include "geometryball.h"
+
+GeometryBall::GeometryBall(float size)
+    :_size(size)
+{
+    _thickness = size * 0.05f;
+}
+
+float GeometryBall::getSize() const
+{
+    return _size;
+}
+
+float GeometryBall::getThickness() const
+{
+    return _thickness;
+}
---
+++ FriskyBall/geometryball.h
@@ -0,0 +1,17 @@
+#ifndef GEOMETRYBALL_H
+#define GEOMETRYBALL_H
+
+
+class GeometryBall
+{
+    float _size = 100;
+    float _thickness = 5;
+public:
+    GeometryBall(float size);
+
+    float getSize() const;
+    float getThickness() const;
+
+};
+
+#endif // GEOMETRYBALL_H
